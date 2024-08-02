9e8d24ec29a7282d3f45f4b52f46ef35a64d1d86
Yoo-SH <ads137485@jbnu.ac.kr>
2024-08-02 14:22:13 +0900

    quaryCommand를 추가함

--- JS-DOM/app.js
+++ JS-DOM/app.js
@@ -2,5 +2,9 @@
 //document.body.children[1].children[0].href = 'https://google.com'
 
 
-let anchorElement = document.getElementById('external-link');
+/* let anchorElement = document.getElementById('external-link');
 anchorElement.href = 'https://google.com'
+ */
+
+let anchorElement = document.querySelector('p a');
+anchorElement.href = 'https://www.instagram.com'
