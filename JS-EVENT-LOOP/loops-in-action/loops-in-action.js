ef2551e2ef30a084c48215c77cbb21266428dc13
Yoo-SH <ads137485@jbnu.ac.kr>
2024-08-04 14:57:34 +0900

    js에서 주사위를 돌리는 프로그램을 추가함

--- JS-EVENT-LOOP/loops-in-action/loops-in-action.js
+++ JS-EVENT-LOOP/loops-in-action/loops-in-action.js
@@ -32,3 +32,71 @@ function highlightLinks()
 }
 
 highlightLinksButtonElement.addEventListener('click',highlightLinks)
+
+
+
+//더미 데이터 출력
+
+const dummyUserData = {
+    Name1 : 'Yoo-SH',
+    Name2 : 'shy',
+    age : 32
+};
+
+
+const displayUserDataButtonElement = document.querySelector('#user-data button')
+
+function displayUserData() {
+    const outputDataElement = document.getElementById('output-user-data')
+
+    outputDataElement.innerHTML = '';
+
+    for(const key in dummyUserData)
+    {
+        const newUserDataListItemElement = document.createElement('li');
+        const outputText = key.toUpperCase() + ': ' + dummyUserData[key]
+        newUserDataListItemElement.textContent = outputText;
+        outputDataElement.append(newUserDataListItemElement);
+
+    }
+}
+
+displayUserDataButtonElement.addEventListener('click', displayUserData);
+
+
+// 주사위 프로그램
+
+const rollDiceButtonElement = document.querySelector('#statistics button');
+
+function rollDice() {
+    return Math.floor(Math.random() * 6 + 1);
+}
+
+function deriveNumberOfDiceRolls() {
+    const targetNumberInputElement = document.getElementById('user-target-number');
+    const diceRollsListElement = document.getElementById('dice-rolls');
+
+    const enteredNumber = parseInt(targetNumberInputElement.value, 10);
+    diceRollsListElement.innerHTML = '';
+
+    let hasRolledTargetNumber = false;
+    let numberOfRolls = 0;
+
+    while (!hasRolledTargetNumber) {
+        const rolledNumber = rollDice();
+        numberOfRolls++;
+        const newRollListItemElement = document.createElement('li');
+        const outputText = 'Roll ' + numberOfRolls + ': ' + rolledNumber;
+        newRollListItemElement.textContent = outputText;
+        diceRollsListElement.append(newRollListItemElement);
+        hasRolledTargetNumber = (rolledNumber === enteredNumber);
+    }
+
+    const outputTotalRollsElement = document.getElementById('output-total-rolls');
+    const outputTargetNumberElement = document.getElementById('output-target-number');
+
+    outputTotalRollsElement.textContent = numberOfRolls; // 수정된 부분
+    outputTargetNumberElement.textContent = enteredNumber;
+}
+
+rollDiceButtonElement.addEventListener('click', deriveNumberOfDiceRolls);
